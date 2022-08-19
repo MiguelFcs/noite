a4dbf0e1d4090c079ccc63eb3915be01d82fb0fb
MiguelFcs <miguelcandido83@gmail.com>
2022-08-19 19:34:37 -0300

    Primeiro commit

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu projeto</title>
+
+    <link href="./css/style.css" rel="stylesheet"/>
+    
+</head>
+<body>
+
+<?php
+    if(10 > 12){
+        echo "<h1> Meu site <h1> ";
+    }else{
+        echo "<script>alert('teste)<script>";
+    }
+    ?>
+    
+    <h1>Hello World!</h1>
+    <p>Bem vindo a aula de programação web</p>
+    <br />
+    <a href="./contato.php">Clique aqui para entrar em contato</a>
+    
+</body>
+</html>
