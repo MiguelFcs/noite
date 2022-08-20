161f54168d6550c1e35247dd67ebe9f6f3ee09b3
MiguelFcs <miguelcandido83@gmail.com>
2022-08-19 21:45:36 -0300

    Aula 19-08-22

---
+++ contato.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Entre em contato</title>
+</head>
+<body>
+
+
+
+    <h1>Formulario de contato</h1>
+    <label>Nome</label>
+    <input type="text" name="nome"/>
+    <button type="submit">Enviar dados</button>
+    <br />
+    <a href="./index.php">Clique aqui para entrar em contato</a>
+    
+    <img src="./Imagens/cerveja.jfif" width="150" height="300" />
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,32 +1,79 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Meu projeto</title>
-
-    <link href="./css/style.css" rel="stylesheet"/>
-    
-</head>
-<body>
-
 <?php
-    if(10 > 12){
-        echo "<h1> Meu site <h1> ";
-    }else{
-        echo "<script>alert('teste)<script>";
-    }
     //Comentarios em PHP - linha
-    /*
+     /*
 
     */
-    ?>
+
+    //echo "Hello World - Meu nome é Miguel";
+    $idade = 21;
+    $nome = "Miguel";
+    $altura = 1.65;
+    $solteiro = true;
+   // $solteiro = 1;
+   // echo $solteiro;
+
+   //echo 10 < 12;
+   //CONST IDADE = 10;
+
+   /*echo IDADE;
+    if(10 > 12)
+    {
+        echo "Mentira";
+    }
+    else
+    {
+        echo "10 não é maior que 12";
+    }
+
+    /*$nota1 = 6;
+    if ($nota1 >= 6)
+    {
+        echo "Aprovado";
+        
+    }else if($nota1 < 6 && $nota1 >= 2)
+        echo "ainda tem chance";
+    }
     
-    <h1>Hello World!</h1>
-    <p>Bem vindo a aula de programação web</p>
-    <br />
-    <a href="./contato.php">Clique aqui para entrar em contato</a>
+    $nota1 = 6;
+    $nota2 = 5;
+    $media = $nota1 + $nota2;
+    $media = $media/2;
+
+    echo $nota1;     
+    echo $nota2;
+    echo $media;
     
-</body>
-</html>
+    if ($media >= 6)
+    {
+        echo "Aprovado";
+
+    } 
+    else
+    {
+        echo "Reprovado";
+    }
+    */
+    $nome = "teste";
+   //echo "<br <br> Mi " . $nome . " nando " . "<br>";
+    do{
+        $contadora++;
+
+    }while(10 < $contadora);
+
+        $cont = 0;
+        while($cont < 10){
+            $cont++;
+
+    }
+
+    for($i = 0; $i < 10; $i++)
+    {
+        if($i % 2 == 0){
+            echo "<p>" . $i . " é par<p>";
+        }else{
+            echo "<p>" . $i . " é impar<p>";
+        }
+    }
+
+
+?>
